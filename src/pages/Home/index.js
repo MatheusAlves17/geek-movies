c1ce64551e6594f2934587119c6c5a6d2db60e59
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-10-24 20:01:41 -0300

    feat: home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import api from '../../services/api';
 
+import './styles.css';
 
 const Home = () => {
+
+    const [movies, setMovies] = useState([]);
+
     useEffect(()=>{
         async function loadMovies(){
             const { data } = await api.get('movie/now_playing',{
@@ -14,14 +19,27 @@ const Home = () => {
                     page: 1
                 }
             })
-            console.log('chegou',data);
+            setMovies(data.results.slice(0,10))
+            console.log('chegou',movies);
         }
         loadMovies()
     },[])
     return(
-        <h1>
-            Home
-        </h1>
+        <div className="container">
+            <div className="movies-list">
+                {
+                    movies.map((item)=>{
+                        return(
+                            <article key={item.id}>
+                                <strong>{item.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original${item.poster_path}`} alt={item.title} className="cover-movie" />
+                                <Link to={`/filmes/${item.id}`}>Ver detalhes</Link>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
