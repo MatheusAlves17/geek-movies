d4a5c21fd0e043e3abdd8b60565eafa55b65894c
Matheus Alves <matheusfrancisco0330@gmail.com>
2023-10-24 19:35:03 -0300

    feat: axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+import api from '../../services/api';
+
+
 const Home = () => {
+    useEffect(()=>{
+        async function loadMovies(){
+            const { data } = await api.get('movie/now_playing',{
+                params: {
+                    api_key: '3a963db3747a0bcd5b77bea8cc7b930b',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            })
+            console.log('chegou',data);
+        }
+        loadMovies()
+    },[])
     return(
         <h1>
             Home
